3a544948cd7ef87add86f1ef62dad29fbdee9307
jojostin04 <yostinxd148@hotmail.com>
2026-02-11 01:27:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,41 +6,18 @@ window.onload = function() {
     const pantallaFinal = document.getElementById("pantallaFinal");
     const musica = document.getElementById("musica");
 
-    if (yesBtn) {
-        yesBtn.addEventListener("click", function() {
-            pantallaInicial.style.display = "none";
-            pantallaFinal.style.display = "flex";
-            musica.play();
-            crearCorazones();
-        });
-    }
+    yesBtn.onclick = function() {
+        pantallaInicial.style.display = "none";
+        pantallaFinal.style.display = "block";
+        musica.play();
+    };
 
-    if (noBtn) {
-        noBtn.addEventListener("mouseover", function() {
-            const x = Math.random() * (window.innerWidth - 120);
-            const y = Math.random() * (window.innerHeight - 60);
-
-            noBtn.style.position = "absolute";
-            noBtn.style.left = x + "px";
-            noBtn.style.top = y + "px";
-        });
-    }
-
-    function crearCorazones() {
-        setInterval(function() {
-            const corazon = document.createElement("div");
-            corazon.className = "corazon";
-            corazon.innerHTML = "💖";
-            corazon.style.left = Math.random() * 100 + "vw";
-            corazon.style.fontSize = (Math.random() * 20 + 20) + "px";
-            document.body.appendChild(corazon);
-
-            setTimeout(function() {
-                corazon.remove();
-            }, 5000);
-
-        }, 300);
-    }
+    noBtn.onmouseover = function() {
+        noBtn.style.position = "absolute";
+        noBtn.style.left = Math.random() * window.innerWidth + "px";
+        noBtn.style.top = Math.random() * window.innerHeight + "px";
+    };
 
 };
+
 
